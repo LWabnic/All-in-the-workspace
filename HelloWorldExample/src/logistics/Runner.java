8fc6e3fa4f14b1bd2a8a975005849cbf26fdcfab
LWabnic <l.wabnic@outlook.com>
2021-09-20 21:29:05 +0100

    Peer programming updates

--- HelloWorldExample/src/logistics/Runner.java
+++ HelloWorldExample/src/logistics/Runner.java
@@ -3,5 +3,9 @@ package logistics;
 public class Runner {
 public static void main(String[] args) {
 	Loading.Arrange();
+//	System.out.println(TestAnything.superBlock("hooopplla"));
+//	System.out.println(TestAnything.superBlock("abbCCCddDDDDeeEEE"));
+//	System.out.println(TestAnything.superBlock("abbCCCCC"));
+//	System.out.println(TestAnything.superBlock(""));
 }
 }
